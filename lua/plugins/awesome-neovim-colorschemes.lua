a723025bf8376417306c3537010cc85fd61cb919
RS Rajput <rsrajput@RSs-Laptop.local>
2024-01-19 22:28:50 +0530

    initial-commit-mba15-nvim

---
+++ lua/plugins/awesome-neovim-colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "AbdelrahmanDwedar/awesome-nvim-colorschemes" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
